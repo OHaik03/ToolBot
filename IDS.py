6b3972081ab354e050f533e3d2f6112dac022843
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-09-24 19:44:02 -0700

    Add files via upload

---
+++ IDS.py
@@ -0,0 +1,18 @@
+Client = 'd29WNzE4WWpESkhzb0pxSTdxdkE6MTpjaQ'
+Client_Secret = 'XYP5ZyXSVu58tQJQpjc8v9GPgeWd1rDjSzH7fyVLFW6oRYPx20'
+
+Access_Token = '1822076500742696963-MFtvFHh71G5OSdysdEvib15hvgztNI'
+AccessT_Secret = 'mlDceUOeFSNDSZmJvVUa9C6R5dx59uWtI9NUAiWMUxd6q'
+
+    #now = datetime.datetime.now()
+    #tweetTime = datetime.datetime(hour = 14, minute = 46, second = 0, microsecond = 0)
+    #schedule.every(1).minutes.do(updateStatus)
+    
+    
+API_KEY = 'cC6GOfhz2UMCfAG9NtHur4jYs'
+API_KEY_SECRET = 'MHxrceLznv5sB1oup8KCUiHquilY3vUxpOK55uEDTyci9TdgSj'
+
+#Bearertoken = 'AAAAAAAAAAAAAAAAAAAAAPQjvQEAAAAAPVwgIm6%2FBkm4LF%2BT%2F%2BKgoINvZL4%3D3no3CsQIljfgq52jdkpXJu36DMDdEsW7ait1BzTMyfykHZedS8'
+
+Consumer_Key = '341KcUxlUT3yOuNHX6AV6aeUM'
+ConsumerKey_Secret = 'Qted2laW3FqgHKhfg02ZvfGuy5cVhPa1UIfRFIeXeNlv1XdEYQ'
---
+++ toolbot.py
@@ -0,0 +1,81 @@
+import tweepy
+import schedule
+import random
+import linecache
+#from IDS import *
+
+Consumer_Key = '341KcUxlUT3yOuNHX6AV6aeUM'
+ConsumerKey_Secret = 'Qted2laW3FqgHKhfg02ZvfGuy5cVhPa1UIfRFIeXeNlv1XdEYQ'
+Client = 'd29WNzE4WWpESkhzb0pxSTdxdkE6MTpjaQ'
+Client_Secret = 'n6A-8ykKJpL9ySXweSwa128xYx9MXGl-7OBoaKAnlPeqGIZ_jj'
+#Bearertoken = 'AAAAAAAAAAAAAAAAAAAAAPQjvQEAAAAAPVwgIm6%2FBkm4LF%2BT%2F%2BKgoINvZL4%3D3no3CsQIljfgq52jdkpXJu36DMDdEsW7ait1BzTMyfykHZedS8'
+Access_Token = '1822076500742696963-cuNmM72NYVzF3kzXLLmT2D9JQhJJJH'
+AccessT_Secret = 'zgwhEM0NvOACKjHz7SE4zWgoK3TALzRBBu8D3cueJ6gY1'
+
+
+#API Credentials for authenticating the bot
+def create_api():
+
+    client = tweepy.Client(
+        consumer_key = Consumer_Key,
+        consumer_secret = ConsumerKey_Secret,
+        access_token = Access_Token,
+        access_token_secret = AccessT_Secret
+        )
+    
+    return client  # Return the client object, not Client string
+
+
+#auth = tweepy.OAuth2UserHandler(client_id= Client, client_secret= Client_Secret)
+#auth.set_access_token(Access_Token, AccessT_Secret)
+
+def getMessage():
+    
+    while True:
+        try:
+        
+            with open("/Users/haik/Desktop/Python Projects/LyricsTool.txt", "r") as sample:
+                #message = linecache.getLine(sample, random.randint(0,492))
+                #message = sample.read(content[random.randint(0,492)])
+                randomInt = random.randint(0, 492)  
+                message = linecache.getline("/Users/haik/Desktop/Python Projects/LyricsTool.txt", randomInt)
+    
+            if not message:
+                message = "testing"    
+            print(message)
+            return message
+    
+        except FileNotFoundError:
+            print("File not found")
+            return "bruh"
+            break
+
+
+#function for tweeting
+def updateStatus(client: tweepy.Client, message: str):   
+    try:     
+        client.create_tweet(text = message)
+        print('Tweeted!')
+        
+    except tweepy.TweepyException as e:
+        print(f'Error: {e}')
+        
+def autoTweet():
+        updateStatus(authentication, getMessage())
+
+#main function, timer is here
+if __name__ == '__main__':
+    authentication = create_api()
+    randomInt = random.randint(0, 492)  
+    print(randomInt)
+
+
+    schedule.every(10).seconds.do(autoTweet)
+    
+    try:
+        while True:
+                schedule.run_pending()
+
+    except KeyboardInterrupt:
+        print('Tweeting Stopped')
+        
