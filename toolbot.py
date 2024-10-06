32032f7550ad2488e7ca4527c50be3823f5528fc
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-10-06 11:22:05 -0700

    Add files via upload

--- toolbot.py
+++ toolbot.py
@@ -1,12 +1,12 @@
 import tweepy
 import schedule
 import linecache
-import IDS
 import saveCount
-#can you shuffle all of the lines so that they are all in different places
+
 
 #API Credentials for authenticating the bot
 def create_api():
+    import IDS
 
     client = tweepy.Client(
         consumer_key = IDS.Consumer_Key,
@@ -15,11 +15,12 @@ def create_api():
         access_token_secret = IDS.AccessT_Secret
         )
     
-    return client  # Return the client object, not Client string
+    return client  # Return the client object
 
-def update(newNum):
+def update(newNum): #meant to save the position in the list of lyrics
     from saveCount import setSave
     saveCount.setSave(newNum)
+    return
     
 
 def getMessage():#gets the lyrics from the file and returns it to the autoTweet() function
@@ -31,15 +32,10 @@ def getMessage():#gets the lyrics from the file and returns it to the autoTweet(
     print(saveCount.saveCounter)
     while True:        
         try:    
-            
-            #sample = open("/Users/haik/Desktop/Python Projects/ShuffledLyricsTool.txt", "r")
-
             message = linecache.getline("/Users/haik/Desktop/Python Projects/ShuffledLyricsTool.txt", counter)
-            #next(sample)
             
             if not message:
                 message = "No more lyrics" 
-                #sample.close()
                    
             print(message) #prints the tweet
             return message#returns the tweet back to autoTweet()
@@ -65,15 +61,13 @@ def autoTweet():#authenticates and gets the message, sends it to the updateStatu
         return #Returns back to callTime()
         
 def callTime():
-    schedule.every(10).seconds.do(autoTweet)
+    schedule.every(6).hours.do(autoTweet)
     
     
-#main function, timer is here
+#main function
 if __name__ == '__main__':
     authentication = create_api()    
     callTime()   
-    setter = 0 
-    update(setter)#sets the 'saveCounter' variable to 0 
     
     try:
         while True:
